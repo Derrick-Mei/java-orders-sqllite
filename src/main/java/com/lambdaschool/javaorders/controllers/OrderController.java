1cc217a9173ece5787517c797427f983514f110b
Derrick Mei <derrick.mei.168@gmail.com>
2019-02-22 05:42:18 -0600

    updated OrderController: added new endpoints and converted old ones

--- src/main/java/com/lambdaschool/javaorders/controllers/OrderController.java
+++ src/main/java/com/lambdaschool/javaorders/controllers/OrderController.java
@@ -4,24 +4,55 @@ import com.lambdaschool.javaorders.models.Order;
 import com.lambdaschool.javaorders.repository.OrderRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
+import java.net.URISyntaxException;
 import java.util.List;
+import java.util.Optional;
 
 @RestController
-@RequestMapping(path = {"/order"}, produces = MediaType.APPLICATION_JSON_VALUE)
+@RequestMapping(path = {"/orders"}, produces = MediaType.APPLICATION_JSON_VALUE)
 public class OrderController
 {
 
     @Autowired
     OrderRepository orderrepos;
 
+    // /orders - return all the orders
     @GetMapping("")
     public List<Order> allOrders()
     {
         return orderrepos.findAll();
+    }
+
+    @GetMapping("/ordnum/{ordnum}")
+    public Order findOrderById(@PathVariable long ordnum)
+    {
+        var foundOrder = orderrepos.findById(ordnum);
+
+        return foundOrder.isPresent() ? foundOrder.get() : null;
+    }
+
+    @PostMapping("")
+    public Order addNewOrder(@RequestBody Order newOrder) throws URISyntaxException
+    {
+        return orderrepos.save(newOrder);
+    }
+
+    @PutMapping("/ordnum/{ordnum}")
+    public Order updateOrder(@RequestBody Order updatedOrder, @PathVariable long ordnum)
+    {
+        Optional<Order> orderById = orderrepos.findById(ordnum);
+        if (orderById.isPresent())
+        {
+            updatedOrder.setId(ordnum);
+            orderrepos.save(updatedOrder);
+            return updatedOrder;
+        }
+        else
+        {
+            return null;
+        }
     }
 
 }
