be4f5dd8a99ae55d28850f7a55601feeaf957f0f
Derrick Mei <derrick.mei.168@gmail.com>
2019-02-22 05:41:45 -0600

    updated AgentController: added new endpoints and converted old ones

--- src/main/java/com/lambdaschool/javaorders/controllers/AgentController.java
+++ src/main/java/com/lambdaschool/javaorders/controllers/AgentController.java
@@ -1,11 +1,10 @@
 package com.lambdaschool.javaorders.controllers;
 
+import com.lambdaschool.javaorders.models.Agent;
 import com.lambdaschool.javaorders.repository.AgentRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -16,8 +15,23 @@ public class AgentController
     @Autowired
     AgentRepository agentrepos;
 
-//agents - Returns all agents with their customers
+    // /agents - return all the agents
     @GetMapping("/agents")
+    public List<Agent> allAgentsOnly()
+    {
+        return agentrepos.findAll();
+    }
+
+    @GetMapping("/agents/agentcode/{agentcode}")
+    public Agent findAgentById(@PathVariable long agentcode)
+    {
+        var agentById = agentrepos.findById(agentcode);
+        return agentById.isPresent() ? agentById.get() : null;
+    }
+
+
+//agents - Returns all agents with their customers
+    @GetMapping("/agents/customers")
     public List<Object[]> findAllAgents()
     {
         return agentrepos.findAgentsAndCustomers();
@@ -28,6 +42,28 @@ public class AgentController
     public List<Object[]> findAgentsAndOrders()
     {
         return agentrepos.findAgentsAndOrders();
+    }
+
+    @PostMapping("/agents")
+    public Agent addNewAgent(@RequestBody Agent newAgent)
+    {
+        return agentrepos.save(newAgent);
+    }
+
+    @PutMapping("/agents/agentcode/{agentcode}")
+    public Agent updateAgent(@RequestBody Agent updatedAgent, @PathVariable long agentcode)
+    {
+        var agentById = agentrepos.findById(agentcode);
+        if (agentById.isPresent())
+        {
+            updatedAgent.setId(agentcode);
+            agentrepos.save(updatedAgent);
+            return updatedAgent;
+        }
+        else
+        {
+            return null;
+        }
     }
 
 //agents/{agentcode} - Deletes an agent if they are not assigned to a customer or order (Stretch Goal)
